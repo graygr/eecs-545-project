7f48e02901544e21ce15f4f7ab9a780a47216726
Justin Wang <43922308+justin31452@users.noreply.github.com>
2022-04-18 02:30:42 -0400

    build feature table and bboxes table

--- features.py
+++ features.py
@@ -31,16 +31,30 @@ def avgContoursArea(contours):
     mean_area = mean_area / len(contours)
     return mean_area
 
-def drawBBox(contours, mean_area, c_frame):
+def drawBBox(contours, c_frame):
     # Compute bounding box and draw it on the frame
+    mean_area = avgContoursArea(contours)
     for c in contours:
         (x,y,w,h) = cv2.boundingRect(c)
         area = cv2.contourArea(c)
         perimeter = cv2.contourArea(c)
-        circularity = None
-        
         if area > mean_area:
             cv2.rectangle(c_frame, (x-10,y-10), (x+10+w, y+10+h), (255, 0, 0), 2)
+    cv2.imshow('Frame', c_frame)
+    cv2.imwrite("media/im_with_keypoints_n_3_gtn_mean_area.png", c_frame)
+
+def extract(contours, c_frame):
+    features = []
+    bboxes = []
+    # Compute bounding box and draw it on the frame
+    for c in contours:
+        (x,y,w,h) = cv2.boundingRect(c)
+        area = cv2.contourArea(c)
+        perimeter = cv2.arcLength(c, True)
+        circularity = 4*np.pi / (perimeter**2)
+        features.append([area, perimeter, circularity])
+        bboxes.append([x,y,w,h])
+    return features, bboxes
 
 
 def main():
@@ -50,16 +64,16 @@ def main():
     while(vr.isOpened()):
         c_frame = combineFrames(vr)
         contours, hierarchy = cv2.findContours(c_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        mean_area = avgContoursArea(contours)
-        drawBBox(contours, mean_area, c_frame)
-        cv2.imshow('Frame', c_frame)
-        cv2.imwrite("media/im_with_keypoints_n_3_gtn_mean_area.png", c_frame)
+        features, bboxes = extract(contours, c_frame)
+        print(len(features))
+        print(len(bboxes))
+        # drawBBox(contours, c_frame)
         # Freeze until any key pressed. Quit on pressing q
-        if cv2.waitKey(0) & 0xFF == ord('q'):
-            break
+        # if cv2.waitKey(0) & 0xFF == ord('q'):
+        #     break
     vr.release()
     cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    main()
+    main() 
