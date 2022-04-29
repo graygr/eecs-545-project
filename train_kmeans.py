51c95be9ea7ce2a8e9bfb5695fd7da4b4c20a4b4
ronangabriel <83051245+ronangabriel@users.noreply.github.com>
2022-04-28 20:18:03 -0400

    Added kernel option

--- train_kmeans.py
+++ train_kmeans.py
@@ -3,7 +3,8 @@ from sklearn.cluster import KMeans
 
 import pickle
 
-fname = 'simple-fg'
+fname = 'complex-bg'
+kernel_flag = 1
 
 def main():
 
@@ -11,10 +12,17 @@ def main():
 
     with open('./pickle/' + fname[:-3] + '_' + fname[-2:] + '_features.pkl', 'rb') as f:
         features = pickle.load(f)
+        if kernel_flag != 0:
+            features = np.reshape(np.linalg.norm(features, axis=1), (-1, 1))
+    
     classifier.fit(features)
 
-    with open('./pickle/kmeans_' + fname[:-3] + '_' + fname[-2:] + '.pkl', 'wb') as f:
-        pickle.dump(classifier, f)
+    if kernel_flag == 0:
+        with open('./pickle/kmeans_' + fname[:-3] + '_' + fname[-2:] + '.pkl', 'wb') as f:
+            pickle.dump(classifier, f)
+    elif kernel_flag == 1:
+        with open('./pickle/kmeans_kernel_' + fname[:-3] + '_' + fname[-2:] + '.pkl', 'wb') as f:
+            pickle.dump(classifier, f)
 
 if __name__ == '__main__':
-    main() 
+    main()
